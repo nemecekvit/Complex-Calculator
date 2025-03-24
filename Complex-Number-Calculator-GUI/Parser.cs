cba70f17c16bd4648f132a38aaa45a130a004942
lokit58 <vitecek73@gmail.com>
2025-03-24 21:45:38 +0100

    Added parser class and connected InputValidator

---
+++ Complex-Number-Calculator-GUI/Parser.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Complex_Number_Calculator_GUI
+{
+    internal class Parser
+    {
+        public Parser(string input)
+        {
+            // Remove all white spaces from the input string
+            input = input.Where(c => !Char.IsWhiteSpace(c)).Aggregate("", (current, c) => current + c);
+            // Validate the input string
+            try{ InputValidator.ValidateInput(input); }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+    }
+}
